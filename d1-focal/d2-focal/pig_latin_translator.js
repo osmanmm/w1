2524860d5f7ed089ab40eb92de74bc3700fa7b43
osmanmm <mohamed.osman@outlook.com>
2021-08-09 01:36:08 +0000

    delete lotide

--- d1-focal/d2-focal/pig_latin_translator.js
+++ d1-focal/d2-focal/pig_latin_translator.js
@@ -1,15 +1,12 @@
-var originalWords = process.argv.slice(2);
-let arg = process.argv;
-var pigLatinWords = [];
-console.log('originalWords', originalWords);
-console.log('arg',arg);
-for (var i = 1; i < originalWords.length; i++) {
+let originalWords = process.argv.slice(2);
+let pigLatinWords = [];
+for (let i = 0; i < originalWords.length; i++) {
+  
   pigLatinWords.push(translateToPigLatin(originalWords[i]));
 }
 
-
+console.log(pigLatinWords.join(' '));
 
 function translateToPigLatin(word) {
-  return word.slice(2, word.length) + word[0] + "ay";
+  return word.slice(1, word.length) + word[0] + "ay";
 }
-console.log(pigLatinWords.join(' '));
